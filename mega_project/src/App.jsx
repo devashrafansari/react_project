3e836363632aa75bac1d20141fc754fce7305627
iashrafansari <yesashrafraza@gmail.com>
2026-08-20 20:16:00 +0530

    store created, components created, input created

--- mega_project/src/App.jsx
+++ mega_project/src/App.jsx
@@ -1,13 +1,38 @@
 
+import { useEffect, useState } from 'react'
+import {useDispatch} from 'react-redux'
+import authService from './appwrite/auth'
+import {login, logout} from './store/authSlice'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 import './App.css'
 
 function App() {
 
-  return (
-    <>
-      <h1>Welcome to Vite + React</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch()
+
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login(userData))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [dispatch])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex_wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
