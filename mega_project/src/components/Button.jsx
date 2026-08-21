5f1b60d2b2fa772f0748a7527cead465dcde2434
iashrafansari <yesashrafraza@gmail.com>
2026-08-21 17:07:19 +0530

    login, postcard, signup components added

--- mega_project/src/components/Button.jsx
+++ mega_project/src/components/Button.jsx
@@ -8,7 +8,9 @@ function Button({
     ...props
 }) {
   return (
-    <button className={`px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className}`} {...props}>
+    <button 
+    type={type}
+    className={`px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className}`} {...props}>
         {children}
     </button>
   )
